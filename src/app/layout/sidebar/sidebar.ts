f0767eea413fa88eb131ff86ec6a7cb0ce012a61
Emanuel Bitencourt <emanuelmagalhaes3m@gmail.com>
2025-12-10 19:08:41 -0300

    feat: implementando sidebar no app

--- src/app/layout/sidebar/sidebar.ts
+++ src/app/layout/sidebar/sidebar.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterLink, RouterLinkActive } from '@angular/router';
+import { RouterLink } from '@angular/router';
 import { SidebarItens } from '../../components/sidebar-itens/sidebar-itens';
 
 @Component({
   selector: 'app-sidebar',
-  imports: [RouterLink, RouterLinkActive, SidebarItens],
+  imports: [RouterLink, SidebarItens],
   templateUrl: './sidebar.html',
   styleUrl: './sidebar.css',
 })
