893ac61c18f766fbe65399c91c7382aaea12b9c6
Emanuel Bitencourt <emanuelmagalhaes3m@gmail.com>
2025-12-10 19:19:17 -0300

    feat: componente lista-chamados

---
+++ src/app/pages/lista-chamados/lista-chamados.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-lista-chamados',
+  imports: [],
+  templateUrl: './lista-chamados.html',
+  styleUrl: './lista-chamados.css',
+})
+export class ListaChamados {
+
+}
