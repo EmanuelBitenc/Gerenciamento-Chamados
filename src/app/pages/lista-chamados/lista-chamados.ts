c403bff4e2056a4a6da12ea0d49b97c9722791c6
Emanuel Bitencourt <emanuelmagalhaes3m@gmail.com>
2025-12-11 18:42:18 -0300

    delete: table chamados

--- src/app/pages/lista-chamados/lista-chamados.ts
+++ src/app/pages/lista-chamados/lista-chamados.ts
@@ -3,10 +3,11 @@ import { ChamadosCardsResume } from '../../components/chamados-cards-resume/cham
 import { TableChamados } from '../../components/table-chamados/table-chamados';
 import { Headerpages } from '../../components/headerpages/headerpages';
 import { FilterListaChamados } from '../../components/filter-lista-chamados/filter-lista-chamados';
+import { CardsChamados } from '../../components/cards-chamados/cards-chamados';
 
 @Component({
   selector: 'app-lista-chamados',
-  imports: [ChamadosCardsResume, TableChamados, Headerpages, FilterListaChamados],
+  imports: [ChamadosCardsResume, TableChamados, Headerpages, FilterListaChamados, CardsChamados],
   templateUrl: './lista-chamados.html',
   styleUrl: './lista-chamados.css',
 })
