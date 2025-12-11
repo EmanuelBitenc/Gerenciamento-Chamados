cb5eb9eb898ffb14042d8a1b472d64166872f55a
Emanuel Bitencourt <emanuelmagalhaes3m@gmail.com>
2025-12-11 14:24:17 -0300

    build:; configuração de cor padrão primeNG

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,16 +2,34 @@ import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/
 import { provideRouter } from '@angular/router';
 import { providePrimeNG } from 'primeng/config';
 import Aura from '@primeuix/themes/aura';
+import { definePreset } from '@primeuix/themes';
 
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 
+const CustomAuraPreset = definePreset(Aura, {
+  // Usa a paleta blue do Tailwind (blue.50 ... blue.950)
+  semantic: {
+    primary: {
+      50: '{blue.50}',
+      100: '{blue.100}',
+      200: '{blue.200}',
+      300: '{blue.300}',
+      400: '{blue.400}',
+      500: '{blue.500}',
+      600: '{blue.600}',
+      700: '{blue.700}',
+      800: '{blue.800}',
+      900: '{blue.900}',
+      950: '{blue.950}',
+    },
+  },
+});
+
 export const appConfig: ApplicationConfig = {
   providers: [
     providePrimeNG({
-      theme: {
-        preset: Aura,
-      },
+      theme: { preset: CustomAuraPreset },
     }),
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes),
