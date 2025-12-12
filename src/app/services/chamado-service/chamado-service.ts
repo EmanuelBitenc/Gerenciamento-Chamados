f53e538d71751eb93c49bed7c6f66f50422ab7d8
Emanuel Bitencourt <emanuelmagalhaes3m@gmail.com>
2025-12-12 14:17:40 -0300

    feature: service get chamados

--- src/app/services/chamado-service/chamado-service.ts
+++ src/app/services/chamado-service/chamado-service.ts
@@ -1,8 +1,17 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Chamado } from '../../model/chamado';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ChamadoService {
-  
+  private apiUrl = 'https://emanuelbitenc.github.io/ImagesProject/datamock.json';
+
+  constructor(private http: HttpClient) {}
+
+  getChamados(): Observable<Chamado[]> {
+    return this.http.get<Chamado[]>(this.apiUrl);
+  }
 }
